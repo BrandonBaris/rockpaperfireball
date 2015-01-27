6dbd0592d40455d213a1aa89373ba543f67f3cea
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-01-26 15:30:14 -1000

    made comments

--- rpf.js
+++ rpf.js
@@ -2,29 +2,27 @@ var playerScore = 0;
 var computerScore = 0; //starts at 0
 
 function play (user){
-  var computer = Math.round(Math.random() * 2); 
-  var player;
-  var result;
-  console.log(computer);
+  var computer = Math.round(Math.random() * 2); // chooses 0 1 or 2?
+  var player; 
+  console.log(computer); // calls the function computer as a variable??
 
   if (user === "rock") {
-    player = 0;
-    console.log("0");
-    
-  } else 
-  if (user === "paper") {
-    player = 1;
-    console.log("1");
-    
-  } else 
-  if (user === "fireball") {
-    player = 2;
-    console.log("2");    
+      player = 0;
+      console.log("0");
+      // if user types in "rock" displays 0   
+  } else if (user === "paper") {
+     player = 1;
+     console.log("1");
+     // if user types in "paper" displays 1   
+  } else if (user === "fireball") {
+      player = 2;
+      console.log("2");    
   } else
-  {
-    console.log("SORRY CANT UNDERSTAND");
+      console.log("SORRY CANT UNDERSTAND");
   }
 
+  // Matrix of 0 : TIE , 
+  // CPUvsME:Player v----MINUS the Computer's choice = 0, 1, -1, 2, or -2 for the CASES
   switch (player - computer) {
    case -2:
       console.log("Wins");
@@ -45,6 +43,6 @@ function play (user){
     default:
       console.log("Try Again");  
   }
-
+  // This is the final printed to see the scores for each variable within the play function
   console.log("Player: ",playerScore," Computer: ", computerScore);
 }
